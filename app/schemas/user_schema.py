dc0c17d7645411a008f55534e9a8dc91bc4aad17
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-14 23:21:16 +0530

    created user response schema

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 
 class UserRegister(BaseModel):
     email: EmailStr
     password: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
