69185cf38ede7620c6e573074a2798deadf17537
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-14 22:00:02 +0530

    fixed created_at default value

--- app/models/models.py
+++ app/models/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy import Column, Integer, String, TIMESTAMP, text
 from ..database import database
 
 
@@ -9,5 +9,5 @@ class User(database.Base):
     email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(
-        TIMESTAMP(timezone=True), server_default=database.func.now(), nullable=False
+        TIMESTAMP(timezone=True), server_default=text("now()"), nullable=False
     )
