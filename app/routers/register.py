28e4648eddf6aaae95d6f0443926221f487a5e29
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-15 16:30:53 +0530

    added check for already register user

--- app/routers/register.py
+++ app/routers/register.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from ..schemas import user_schema
 from ..models import models
@@ -11,10 +11,15 @@ router = APIRouter(prefix="/register", tags=["register"])
 
 @router.post("/", response_model=user_schema.UserResponse)
 def register(user: user_schema.UserRegister, db: Session = Depends(database.get_db)):
+    email_query = db.query(models.User).filter(models.User.email == user.email).first()
+    if email_query:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Email {user.email} already registered",
+        )
     hashed_password = crypto.hash(user.password)
     user.password = hashed_password
     new_user = models.User(**user.dict())
-    print(user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
