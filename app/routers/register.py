196e1eaac7c63c1e48112b466cb616e9fec0b670
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-15 11:22:31 +0530

    extracted the register route from user.py

---
+++ app/routers/register.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..schemas import user_schema
+from ..models import models
+from ..database import database
+from ..utils import crypto
+
+
+router = APIRouter(prefix="/register", tags=["register"])
+
+
+@router.post("/", response_model=user_schema.UserResponse)
+def register(user: user_schema.UserRegister, db: Session = Depends(database.get_db)):
+    hashed_password = crypto.hash(user.password)
+    user.password = hashed_password
+    new_user = models.User(**user.dict())
+    print(user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
