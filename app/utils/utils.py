94e4f930f82799470b117c131fda55c83930fa4a
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-15 22:06:18 +0530

    implemented the logic to fetch calories as per text

---
+++ app/utils/utils.py
@@ -0,0 +1,21 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = os.getenv("CALORIES_URL")
+APP_ID = os.getenv("APP_ID")
+API_KEY = os.getenv("API_KEY")
+
+headers = {"Content-Type": "application/json", "x-app-id": APP_ID, "x-app-key": API_KEY}
+
+
+def get_calories(text: str):
+    data = {"query": text}
+    response = requests.post(url, headers=headers, json=data)
+    result = response.json().get("foods")
+    calories = 0
+    for food in result:
+        calories = calories + food.get("nf_calories")
+    return int(calories)
