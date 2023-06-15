fe45e9eb42a8e1a8c5a44b6c58f9bb488e38aa00
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-15 16:25:15 +0530

    added the route for register and entries

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 
 from .models import models
 from .database import database
-from .routers import user
+from .routers import user, entries, register
 
 models.database.Base.metadata.create_all(bind=database.engine)
 
@@ -14,4 +14,6 @@ def root():
     return {"status": "running"}
 
 
+app.include_router(register.router)
 app.include_router(user.router)
+app.include_router(entries.router)
