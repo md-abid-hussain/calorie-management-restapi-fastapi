ba4b9d9a5ba89aa40a55f38599303a3d8fee71d4
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-15 17:08:46 +0530

    created login route

---
+++ app/routers/auth.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from ..database import database
+from ..models import models
+from ..utils import oauth2, crypto
+
+router = APIRouter(tags=["Authentication"])
+
+
+@router.post("/login")
+def login(
+    user_credential: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
+):
+    user = (
+        db.query(models.User)
+        .filter(models.User.email == user_credential.username)
+        .first()
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid Credentials"
+        )
+
+    if not crypto.verify(user_credential.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Incorrect Password"
+        )
+
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
