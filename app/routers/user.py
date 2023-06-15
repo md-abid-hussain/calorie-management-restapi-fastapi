960da5d6e38fc6801a487389c2626537484d8c4a
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-15 09:18:29 +0530

    added hash function to encrypt password

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from ..schemas import user_schema
 from ..models import models
 from ..database import database
+from ..utils import crypto
 
 router = APIRouter(
     prefix="/users",
@@ -12,6 +13,8 @@ router = APIRouter(
 
 @router.post("/register", response_model=user_schema.UserResponse)
 def register(user: user_schema.UserRegister, db: Session = Depends(database.get_db)):
+    hashed_password = crypto.hash(user.password)
+    user.password = hashed_password
     new_user = models.User(**user.dict())
     print(user.dict())
     db.add(new_user)
