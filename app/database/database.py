172c617d2151ae4b3b74c8deb076ea5d428ec207
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-17 20:39:07 +0530

    added method to enable foreign key

--- app/database/database.py
+++ app/database/database.py
@@ -1,5 +1,6 @@
 import os
-from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
@@ -9,8 +10,16 @@ load_dotenv()
 SQLALCHEMY_DATABASE_URL = os.getenv("SQLITE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
