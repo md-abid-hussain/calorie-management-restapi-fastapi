93774abff8f7ad84787350ccbafb6a498aa95dfd
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-17 16:16:48 +0530

    created base schema for user crud

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -3,18 +3,22 @@ from datetime import datetime
 from enum import Enum
 
 
-class UserRegister(BaseModel):
-    email: EmailStr
-    password: str
-
-
-class UserResponse(BaseModel):
+class UserBase(BaseModel):
     id: int
     email: EmailStr
     created_at: datetime
 
     class Config:
         orm_mode = True
+
+
+class UserRegister(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserResponse(UserBase):
+    role: str
 
 
 class UserCreate(UserRegister):
