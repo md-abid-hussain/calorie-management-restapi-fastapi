8690dba7d5c129f01118ab6b6c20ddbc1881ddc2
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-17 20:39:31 +0530

    added the route for user setting

--- app/main.py
+++ app/main.py
@@ -2,19 +2,14 @@ from fastapi import FastAPI, Depends
 
 from .models import models
 from .database import database
-from .routers import user, entries, register, auth
+from .routers import user, entries, register, auth, setting
 
 models.database.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
-
-@app.get("/")
-def root():
-    return {"status": "running"}
-
-
 app.include_router(register.router)
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(entries.router)
+app.include_router(setting.router)
