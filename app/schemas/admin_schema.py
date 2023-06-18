34e9fec01b8501e75bb22abb2e030995803301e3
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-18 12:29:59 +0530

    created schema for entry response and create

--- app/schemas/admin_schema.py
+++ app/schemas/admin_schema.py
@@ -8,3 +8,15 @@ class UserSettingResponse(setting_schema.SettingResponse):
 
 class UserSettingCreate(setting_schema.SettingCreate):
     pass
+
+
+class UserEntriesResponse(entry_schema.EntryResponse):
+    owner: user_schema.UserResponse
+
+
+class EntryResponse(entry_schema.EntryResponse):
+    pass
+
+
+class EntryCreate(entry_schema.EntryCreate):
+    pass
