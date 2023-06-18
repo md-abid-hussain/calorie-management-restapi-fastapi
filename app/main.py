0dddc50757a34132a39c83295cbc3ac84b0e2616
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-18 12:39:01 +0530

    added the entry & setting of admin_crud to main

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, Depends
 from .models import models
 from .database import database
 from .routers import user, entries, register, auth, setting
+from .routers.admin_crud import entry as entry_crud, setting as setting_crud
 
 models.database.Base.metadata.create_all(bind=database.engine)
 
@@ -10,6 +11,8 @@ app = FastAPI()
 
 app.include_router(register.router)
 app.include_router(auth.router)
+app.include_router(entry_crud.router)
+app.include_router(setting_crud.router)
 app.include_router(user.router)
 app.include_router(entries.router)
 app.include_router(setting.router)
