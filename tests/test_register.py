2455de2dcedc8392f2c4699de796f9907da7e96f
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-06-19 13:04:52 +0530

    completed test cases for register route

--- tests/test_register.py
+++ tests/test_register.py
@@ -1,5 +1,4 @@
 from app.schemas import user_schema
-from .database import client, session
 
 
 def test_register_user(client):
@@ -9,3 +8,11 @@ def test_register_user(client):
     new_user = user_schema.UserBase(**res.json())
     assert new_user.email == "fast@mail.com"
     assert res.status_code == 201
+
+
+def test_register_user_error(client, test_user):
+    res = client.post(
+        "/register/",
+        json={"email": test_user["email"], "password": test_user["password"]},
+    )
+    assert res.status_code == 400
